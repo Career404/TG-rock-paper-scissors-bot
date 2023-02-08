51033b5959a2d30815288fec2c934dd124394129
Career404 <105638831+Career404@users.noreply.github.com>
2023-02-08 18:14:53 +0400

    feat: use Telegraf, dotenv, nodemon

--- src/bot.ts
+++ src/bot.ts
@@ -1,36 +1,40 @@
-import { Bot, Keyboard } from 'grammy'
+import { Markup, Telegraf } from 'telegraf'
 import { RPSMatch, BotMatch } from './RPSLogic.js'
-import { BOT_TOKEN } from './secret.js'
+import * as dotenv from 'dotenv'
+dotenv.config()
 console.log('Bot online')
 
-const bot = new Bot(BOT_TOKEN)
-const keyboard = new Keyboard().text('🗿').text('📃').text('✂️')
-function emojiToText(e) {
-	return e === '🗿'
-		? 'rock'
-		: e === '📃'
-		? 'paper'
-		: e === '✂️'
-		? 'scissors'
-		: null
-}
-function textToEmoji(text) {
-	return text === 'rock'
-		? '🗿'
-		: text === 'paper'
-		? '📃'
-		: text === 'scissors'
-		? '✂️'
-		: null
+const bot = new Telegraf(process.env.BOT_TOKEN)
+const keyboard = Markup.keyboard([['🗿'], ['📃'], ['✂️']])
+const helpers = {
+	emojiToText: (e) => {
+		return e === '🗿'
+			? 'rock'
+			: e === '📃'
+			? 'paper'
+			: e === '✂️'
+			? 'scissors'
+			: null
+	},
+	textToEmoji: (text) => {
+		return text === 'rock'
+			? '🗿'
+			: text === 'paper'
+			? '📃'
+			: text === 'scissors'
+			? '✂️'
+			: null
+	},
 }
 
-bot.command('bot', (ctx) => {
-	ctx.reply('Bot-paper-scissors - go!', { reply_markup: keyboard })
+bot.command('bot', async (ctx, next) => {
+	ctx.reply('Bot-paper-scissors - go!', keyboard)
+	await next()
 })
-bot.hears(/^(🗿|📃|✂️)$/, (ctx) => {
-	const newMatchResult = BotMatch(emojiToText(ctx.msg.text))
-	ctx.reply(textToEmoji(newMatchResult[1]))
-	ctx.reply(newMatchResult[0])
+bot.hears(/^(🗿|📃|✂️)$/, async (ctx) => {
+	const newMatchResult = BotMatch(helpers.emojiToText(ctx.message.text))
+	await ctx.reply(helpers.textToEmoji(newMatchResult[1]))
+	await ctx.reply(newMatchResult[0])
 })
 
-bot.start()
+bot.launch()
