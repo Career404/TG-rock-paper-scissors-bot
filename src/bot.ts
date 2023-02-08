a1bff02027f7e9bce9ba962cdad4902a7de97271
Career404 <105638831+Career404@users.noreply.github.com>
2023-02-08 18:36:21 +0400

    feat: add dotenv

--- src/bot.ts
+++ src/bot.ts
@@ -1,35 +1,38 @@
 import { Bot, Keyboard } from 'grammy'
 import { RPSMatch, BotMatch } from './RPSLogic.js'
-import { BOT_TOKEN } from './secret.js'
+import * as dotenv from 'dotenv'
+dotenv.config()
 console.log('Bot online')
 
-const bot = new Bot(BOT_TOKEN)
+const bot = new Bot(process.env.BOT_TOKEN)
 const keyboard = new Keyboard().text('🗿').text('📃').text('✂️')
-function emojiToText(e) {
-	return e === '🗿'
-		? 'rock'
-		: e === '📃'
-		? 'paper'
-		: e === '✂️'
-		? 'scissors'
-		: null
-}
-function textToEmoji(text) {
-	return text === 'rock'
-		? '🗿'
-		: text === 'paper'
-		? '📃'
-		: text === 'scissors'
-		? '✂️'
-		: null
+const helpers = {
+	emojiToText: (e) => {
+		return e === '🗿'
+			? 'rock'
+			: e === '📃'
+			? 'paper'
+			: e === '✂️'
+			? 'scissors'
+			: null
+	},
+	textToEmoji: (text) => {
+		return text === 'rock'
+			? '🗿'
+			: text === 'paper'
+			? '📃'
+			: text === 'scissors'
+			? '✂️'
+			: null
+	},
 }
 
 bot.command('bot', (ctx) => {
 	ctx.reply('Bot-paper-scissors - go!', { reply_markup: keyboard })
 })
 bot.hears(/^(🗿|📃|✂️)$/, (ctx) => {
-	const newMatchResult = BotMatch(emojiToText(ctx.msg.text))
-	ctx.reply(textToEmoji(newMatchResult[1]))
+	const newMatchResult = BotMatch(helpers.emojiToText(ctx.msg.text))
+	ctx.reply(helpers.textToEmoji(newMatchResult[1]))
 	ctx.reply(newMatchResult[0])
 })
 
